be09f6f9652a95186fd28610efce09fcdf12bb04
luizfelipebraga <ruivolfb@gmail.com>
2021-10-08 21:39:33 -0300

    feat(initial commit): create a project to start cloning the uber project

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { store } from './store.js';
+import { Provider } from 'react-redux';
+
+//Set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Hi Uber app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
