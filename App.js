6ca2984d402e901087b71c6f3586adbfbc08ed8b
luizfelipebraga <ruivolfb@gmail.com>
2021-10-09 18:38:56 -0300

    feat(react-navigation): configuring react-stack-navigator
    implement stack navigator (navegacao em pilha) - APP.js

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+//react-redux
 import { store } from './src/store.js';
 import { Provider } from 'react-redux';
+ 
+//pages
 import { HomeScreen } from './src/screens/HomeScreen.js';
+import { MapScreen } from './src/screens/MapScreen';
+
+//navigator
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+
+        <SafeAreaProvider>
+
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen} options={{
+                headerShown: false,
+              }} />
+
+            <Stack.Screen
+              name="EatsScreen"
+              component={MapScreen} options={{
+                headerShown: false,
+              }} />
+          </Stack.Navigator>
+
+        </SafeAreaProvider>
+
+      </NavigationContainer>
     </Provider>
   );
 }
